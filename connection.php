54063ab85b12b9103114f3979d5aadfef4010ca9
Subham Kumar <kumarshubham209@gmail.com>
2019-04-16 22:46:29 +0530

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,8 @@
+<?php
+$a=mysqli_connect("localhost","root","","data");
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+
+?>
---
+++ subscribe.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Subscribed!!!</title>
+</head>
+<body style="background-color: black">
+	<center>
+		<h1 style="color: white">Thanks for subscribing</h1>
+		<h3 style="color: white">We will catch you :)</h3>
+		<h3 style="color: white"><a href="index.php">Return to home</a></h3>
+	</center>
+</body>
+</html>
